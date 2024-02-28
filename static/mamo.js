1f71dd08844eafb1452028283bc4b0c2d447ce93
hotrooibos <45525110+hotrooibos@users.noreply.github.com>
2024-02-29 00:05:46 +0100

    webui : wip edit compare before/after

--- static/mamo.js
+++ static/mamo.js
@@ -18,10 +18,6 @@ const fieldAliasDel = doc.querySelector('#id_del');
 const dialogDel     = doc.querySelector('#dialog-del');
 
 
-// const listTest      = doc.querySelector('#listTest');
-// const testList      = doc.querySelector('#testList');
-
-
 
 function showInfobox(msg) {
     var boxesArr = doc.querySelectorAll('.msgbox');
@@ -106,6 +102,100 @@ async function delRedir(form) {
 }
 
 
+// Function used to lock editable cells (<td>)
+// as soon as the user click anywhere out of the
+// row currently edited
+function lockRow(e) {
+    // Identify currently clicked element
+    let ele = e.target;
+    let trParent = ele.closest('tr');
+    // Identify row/cells under edition
+    let editedTr = doc.querySelector('tr[edition]');
+    let editedTdArr = doc.querySelectorAll('td[contenteditable]');
+
+    // If click event happened anywhere but on
+    // the currently edited row elements: 
+    // - Process the content modification
+    // - Terminate table edition
+    // - Remove the click listener triggering this function
+    if (!trParent || !trParent.hasAttribute('edition')) {
+        // Process content modification
+        // Get content from edited cells
+        const editedContent = {};
+
+        editedTdArr.forEach((tdItem, index) => {
+            aliasItem = tdItem.dataset.aliasItem;
+            editedContent[aliasItem] = tdItem.innerHTML;
+        });
+
+        let editedMsg = JSON.stringify(editedContent);
+        showInfobox("edited : " + editedMsg);
+        
+        // Remove the table edition
+        editedTr.removeAttribute('edition');
+
+        editedTdArr.forEach((tdItem, index) => {
+            tdItem.removeAttribute('contenteditable','');
+            tdItem.removeAttribute('class');
+        });
+
+        window.removeEventListener('click', lockRow);
+    }
+}
+
+// Function called when clicking an edit row btn
+// Makes the row editable
+function editRow(e) {
+    e.preventDefault();
+    console.log(e.currentTarget.myParam);
+    let ele = e.target;
+    let parent = ele.parentElement.closest('tr');
+    let tdArr = parent.querySelectorAll('td');
+
+    // If editable row/cells exists, lock them
+    // before making the current selection editable
+    let editedTr = doc.querySelector('tr[edition]');
+    let editedTdArr = doc.querySelectorAll('td[contenteditable]');
+
+    if (editedTr) {
+        // Remove the table edition
+        editedTr.removeAttribute('edition');
+
+        editedTdArr.forEach((tdItem, index) => {
+            tdItem.removeAttribute('contenteditable','');
+            tdItem.removeAttribute('class');
+        });
+    }
+
+    parent.setAttribute('edition', '');
+
+    // Get data from the row to be edited and
+    // make useful cells' (<td>) content editable
+    const content = {};
+
+    tdArr.forEach((tdItem, index) => {
+        switch (index) {
+            case 0:
+            case 2:
+            case 3:
+                tdItem.setAttribute('contenteditable','');
+                tdItem.setAttribute('class', 'td-editable');
+                aliasItem = tdItem.dataset.aliasItem;
+                content[aliasItem] = tdItem.innerHTML;
+                break;
+            default:
+                break;
+        }
+    });
+
+    let editedMsg = JSON.stringify(content);
+    showInfobox("original : " + editedMsg);
+
+    // Add a click listener to anywhere on the document
+    window.addEventListener("click", lockRow);
+}
+
+
 /*
 
 Event listeners
@@ -133,90 +223,11 @@ btnsDel.forEach(ele => ele.addEventListener('click', (e) => {
     dialogDel.showModal();
 }));
 
-// Function used to lock editable cells (<td>)
-// as soon as the user click anywhere out of the
-// row currently edited
-function lockRow(e) {
-    // Identify currently clicked element
-    let ele = e.target;
-    let trParent = ele.closest('tr');
 
-    // If click event happened anywhere but on
-    // the currently edited row elements: 
-    // - Process the content modification
-    // - Terminate table edition
-    // - Remove the click listener triggering this function
-    if (!trParent || !trParent.hasAttribute('edition')) {
-        // Identify row/cells under edition
-        let editedTr = doc.querySelector('tr[edition]');
-        let editedTdArr = doc.querySelectorAll('td[contenteditable]');
-
-        // Process content modification
-        // Get content from edited cells
-        const editedContent = {};
-
-        editedTdArr.forEach((tdItem, index) => {
-            aliasItem = tdItem.dataset.aliasItem;
-            editedContent[aliasItem] = tdItem.innerHTML;
-        });
-
-        let editedMsg = JSON.stringify(editedContent);
-        showInfobox(editedMsg);
-        
-        // Remove the table edition
-        editedTr.removeAttribute('edition');
-
-        editedTdArr.forEach((tdItem, index) => {
-            tdItem.removeAttribute('contenteditable','');
-            tdItem.removeAttribute('class');
-        });
-
-        window.removeEventListener('click', lockRow);
-    }
-}
-
-function editRow(e) {
-    e.preventDefault();
-    let ele = e.target;
-    let parent = ele.parentElement.closest('tr');
-    let tdArr = parent.querySelectorAll('td');
-
-    // TODO s'il existe des cells editable, clore leur edition avant d'ouvrir
-    // cette nouvelle edition
-    let editedTr = doc.querySelector('tr[edition]');
-    let editedTdArr = doc.querySelectorAll('td[contenteditable]');
-
-    if (editedTr) {
-        // Remove the table edition
-        editedTr.removeAttribute('edition');
-
-        editedTdArr.forEach((tdItem, index) => {
-            tdItem.removeAttribute('contenteditable','');
-            tdItem.removeAttribute('class');
-        });
-    }
-
-    parent.setAttribute('edition', '');
-
-    // Make useful cells' (<td>) content
-    // editable for the edited row
-    tdArr.forEach((arrayItem, index) => {
-        switch (index) {
-            case 0:
-            case 2:
-            case 3:
-                arrayItem.setAttribute('contenteditable','');
-                arrayItem.setAttribute('class', 'td-editable');
-                break;
-            default:
-                break;
-        }
-    });
-
-    window.addEventListener("click", lockRow);
-}
-
-btnsEdit.forEach(ele => ele.addEventListener('click', editRow));
+btnsEdit.forEach(ele => {
+    ele.addEventListener('click', editRow);
+    ele.myParam = "text_";
+});
 
 dialogDel.addEventListener('close', (e) => {
     if (dialogDel.returnValue === "yes") {
@@ -265,29 +276,3 @@ delForm.addEventListener('submit', (e) => {
     newDelForm = JSON.stringify(Object.fromEntries(newDelForm));
     delRedir(newDelForm);
 });
-
-
-
-// // Tests
-// var testDict = {
-//     "001": {
-//         "name": "Test",
-//         "date": "",
-//         "alias": "001@tical.fr",
-//         "to": "antoine@marzin.org"
-//     },
-//     "002": {
-//         "name": "",
-//         "date": "",
-//         "alias": "002@tical.fr",
-//         "to": "antoine@marzin.org"
-//     }
-// }
-
-// listTest.addEventListener('click', (e) => {
-//     console.log('Type : ' + typeof(testDict));
-
-//     for (const key of Object.keys(testDict)) { 
-//         console.log(key + ": " + JSON.stringify(testDict[key])); 
-//      };
-// });
